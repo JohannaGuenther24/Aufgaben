12beeb04046ff533e6e9095835b7dc3f208902a9
Johanna <johanna.guenther@btc-ag.com>
2024-09-20 13:59:13 +0200

    die ersten Aufgaben

---
+++ src/main/java/com/btcag/bootcamp/PrimeTest.java
@@ -0,0 +1,34 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class PrimeTest {
+    public static void main(String[] args) {
+        System.out.println("Bitte Zahl für den Primetest eingeben.");
+        Scanner scanner = new Scanner(System.in);
+        int eingabe = scanner.nextInt();
+        int teiler = eingabe - 1;
+        int zaehler = eingabe -1;
+
+        while (zaehler > 1) {
+            int rest = eingabe % teiler;
+            //System.out.println(eingabe+ "" +teiler);
+            teiler--;
+            zaehler--;
+            //System.out.println(rest);
+
+            while (rest == 0) {
+                zaehler = 0;
+                rest = 1;
+                System.out.println("Ergebnis: false");
+            }
+            while (zaehler == 2) {
+                System.out.println("Ergebnis: true");
+                zaehler = 1;
+
+            }
+        }
+
+
+    }
+}
