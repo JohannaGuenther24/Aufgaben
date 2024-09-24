09d24bcf3735df3326d1aaacc19914217e8e3b56
Johanna <johanna.guenther@btc-ag.com>
2024-09-24 14:05:54 +0200

    Fertigstellung Zahlen raten

---
+++ src/main/java/com/btcag/bootcamp/GuessNumber.java
@@ -0,0 +1,47 @@
+package com.btcag.bootcamp;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        int zaehler = 1;
+        int reichweite = 0;
+        int bit = 1;
+        int versuche = 1;
+        boolean auswertung = false;
+
+        System.out.println("Bitte gebe die Zahlengröße in Bits an:");
+        int bits = sc.nextInt();
+
+        while (zaehler <= bits) {
+            reichweite = reichweite + bit;
+            bit = bit * 2;
+            zaehler++;
+        }
+
+        long zahl = random.nextLong(reichweite);
+
+
+        while (auswertung == false) {
+            System.out.println(versuche + ". Versuch: Bitte eine Zahl im Bereich 0 und " + reichweite + " angeben:");
+            int raten = sc.nextInt();
+            while (raten == zahl) {
+                auswertung = true;
+                System.out.println("Super!");
+                break;
+            }
+            versuche++;
+            while (raten < zahl) {
+                System.out.println("Die gesuchte Zahl ist groesser!\n");
+                break;
+            }
+            while (raten > zahl){
+                System.out.println("Die gesuchte Zahl ist kleiner!\n");
+                break;
+            }
+        }
+    }
+}
